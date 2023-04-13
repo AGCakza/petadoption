75cce25432106571f0f2017d69151ffd74dfd187
Avtandil Katsadze <avtandilk61@gmail.com>
2023-04-13 13:21:19 +0200

    get rid of some console log

--- app/api/friends/[id]/route.ts
+++ app/api/friends/[id]/route.ts
@@ -8,7 +8,6 @@ import { NextRequest, NextResponse } from "next/server"
 export async function POST(_req: NextRequest, { params }: { params: { id: string } }) {
     const session = await getServerSession(authOptions)
     await dbConnect()
-    console.log([session?.user.id, params.id])
     const friendship = await friendshipServices.addFriend({between: [session?.user.id, params.id], from: session?.user.id})
 
     return NextResponse.json({ status: 'OK', friendship })
