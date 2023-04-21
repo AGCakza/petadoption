5de38ff23a3312f25a1c8733f6f78c285f2e7410
Avtandil Katsadze <avtandilk61@gmail.com>
2023-04-21 20:17:47 +0200

    safari tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,18 +9,7 @@ import { Session } from 'next-auth'
 import { SessionProvider, useSession } from 'next-auth/react'
 import { AppDataContextProvider } from '@/context/AppDataContext'
 import { PetsDataContextProvider } from '@/context/PetsDataContext'
-
-export const metadata = {
-  title: 'Petty',
-  description: 'Welcome to Petty',
-  "mobile-web-app-capable": "yes",
-  "apple-mobile-web-app-capable": "yes",
-  "apple-mobile-web-app-capable": "yes",
-  "mobile-web-app-capable": "yes",
-  "apple-mobile-web-app-status-bar-style": "#FFE5CA",
-  "theme-color": "#FFE5CA",
-  "apple-mobile-web-app-title": "Petty"
-}
+import Head from 'next/head'
 
 export default function Root({
   children,
@@ -53,6 +42,16 @@ const RootLayout = ({
 
   return (
     <html lang="en">
+      <Head>
+        <title>Petty</title>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#FFE5CA" />
+        <meta name="theme-color" content="#FFE5CA" />
+        <meta name="apple-mobile-web-app-title" content="Petty" />
+      </Head>
       <body>
         <div className={styles.root}>
           {children}
